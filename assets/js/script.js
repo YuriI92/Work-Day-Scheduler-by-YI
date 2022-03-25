77ab512fabdd9fae3a23304895abe52c70adfc8d
Yurippe <yurichikawa1992@gmail.com>
2022-03-25 14:44:15 -0400

    add ability to put its color back after editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,32 @@
-// var workStart = 9;  // start time
-// var workEnd = 17;  // end time
-
 // show current day and date in the header
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 // create timeblocks
 var createTimeblocks = function(start, end) {
-    // create a container
-    var timeblocks = $("<div>").addClass("row");
-
     // create each timeblocks element until it reaches end time
     for (var i = start; i < (end + 1); i++) {
+        // create a container
         var timeBlockEl = $("<div>")
-            .addClass("hour col-2 col-md-1 py-2")
+            .addClass("block-container row");
+
+        var timeEl = $("<time>")
+            .addClass("hour col-2 col-md-1 px-1 py-3")
+            .attr("datetime", moment(i, "HH").format("YYYY MM DD kk:mm"))
             .text(moment(i, "HH").format("HA"));
-        var eventBlockEl = $("<p>")
-            .addClass("description col-8 col-md-10 p-1")
+        var eventEl = $("<p>")
+            .addClass("description col-8 col-md-10 p-2")
             .attr("id", "event");
         var saveBtn = $("<button>")
             .addClass("saveBtn col-2 col-md-1");
         var iconEl = $("<i>")
-            .addClass("ri-save-2-fill")
+            .addClass("ri-save-2-fill");
         saveBtn.append(iconEl);
 
-        // get current time
-        var currentTime = moment().format("YYYY MM DD kk:mm");
-        var timeblockTime = moment(i, "HH").format("YYYY MM DD kk:mm");
-        // console.log(currentTime);
-        // console.log(timeblockTime);
-        if (moment(timeblockTime).isBefore(currentTime)) {
-            eventBlockEl.addClass("past");
-        } else if (moment(timeblockTime).isSame(currentTime)) {
-            eventBlockEl.addClass("present");
-        } else {
-            eventBlockEl.addClass("future");
-        }
+        checkCurrentTime(i, eventEl);
 
-        timeblocks.append(timeBlockEl, eventBlockEl, saveBtn);
-        
+        timeBlockEl.append(timeEl, eventEl, saveBtn);
+        $("#timeblocks-container").append(timeBlockEl);
     }
-
-    $("#timeblocks-container").append(timeblocks);
 }
 
 // change to textarea when clicked #event
@@ -50,7 +36,7 @@ $("#timeblocks-container").on("click", "p", function() {
         .text()
         .trim();
     var textInput = $("<textarea>")
-        .addClass("event-text col-8 col-md-10 p-1")
+        .addClass("event-text col-8 col-md-10 p-2")
         .val(text);
     $(this).replaceWith(textInput);
 
@@ -63,11 +49,30 @@ $("#timeblocks-container").on("blur", "textarea", function() {
     var text = $(this)
         .val();    
     var eventEl = $("<p>")
-        .addClass("description col-8 col-md-10 p-1")
+        .addClass("description col-8 col-md-10 p-2")
         .attr("id", "event")
         .text(text);
+    var time = $(this).parent().find(".hour").text().trim();
+    
+    checkCurrentTime(time, eventEl);
+    
     $(this).replaceWith(eventEl);
 });
+
+var checkCurrentTime = function(time, eventEl) {
+    // get current time
+    var currentTime = moment().format("YYYY MM DD kk:mm");
+    var timeblockTime = moment(time, "HH").format("YYYY MM DD kk:mm");
+    // console.log(currentTime);
+    // console.log(timeblockTime);
+    if (moment(timeblockTime).isBefore(currentTime, "hour")) {
+        eventEl.addClass("past");
+    } else if (moment(timeblockTime).isSame(currentTime, "hour")) {
+        eventEl.addClass("present");
+    } else {
+        eventEl.addClass("future");
+    }
+}
 
 // parameters indicates business hours' start time and end time
 createTimeblocks(9, 17);
